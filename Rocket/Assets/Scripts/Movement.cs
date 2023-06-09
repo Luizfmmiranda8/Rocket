25d53404140838f46b211a7b222f9675048d70a8
Luizfmmiranda8 <luizfmmiranda@gmail.com>
2023-06-09 12:24:23 -0300

    Create Prefabs
    Create ground, launch, landing and Rocket prefabs

---
+++ Rocket/Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    #region VARIABLES
+    #endregion
+
+    #region EVENTS
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region METHODS
+    #endregion
+}
