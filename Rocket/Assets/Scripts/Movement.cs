98f0d57224eb58621f64390a2d17e08307a47972
Luizfmmiranda8 <luizfmmiranda@gmail.com>
2023-06-19 11:55:21 -0300

    Move system

--- Rocket/Assets/Scripts/Movement.cs
+++ Rocket/Assets/Scripts/Movement.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     #region VARIABLES
+    [Header("Physics")]
+    Rigidbody rocketRigidbody;
+    [Header("Movement")]
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 100f;
     #endregion
 
     #region EVENTS
+    void Start()
+    {
+        rocketRigidbody = GetComponent<Rigidbody>();
+    }
     void Update()
     {
-        
+        Thrust();
+        Rotation();
     }
     #endregion
 
     #region METHODS
+    void Thrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rocketRigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void Rotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotation)
+    {
+        transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+    }
     #endregion
 }
